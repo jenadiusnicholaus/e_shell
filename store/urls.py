d2388d775b4f7db674039b6f2ce4da47c3aee827
ucodecotz <ucode8888@gmail.com>
2020-12-04 13:04:01 +0300

    fixed sub sub categories bag in html

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('product/', views.ShopProduct.as_view(), name="product"),
-    path('product_details/<int:pk>/', views.Product_details.as_view(), name="product_details"),
+    path('product_details/<int:pk>/', views.ProductDetails.as_view(), name="product_details"),
     path('add_to_cart/<int:pk>/', views.add_to_cart, name="add_to_cart"),
     path('cart/', views.CartIterms.as_view(), name="cart"),
     path('remove_from_cart/<int:pk>/', views.remove_from_cart, name="remove_from_cart"),
@@ -15,6 +15,7 @@ urlpatterns = [
     # Delivery
     path('add_delivery_info/', views.AddDeliveryInfo.as_view(), name="add_delivery_info"),
     path('sub_categories/<int:pk>/', views.SubCategoriesDateils.as_view(), name="sub_categories"),
+    path('sub_sub_categories/<int:pk>/', views.SubSubCategoriesDateils.as_view(), name="sub_sub_categories"),
     path('search/', views.search_product, name="search"),
 
     path('edit_product/<int:pk>', views.edit_product, name="edit_product"),
