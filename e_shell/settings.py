33e7f08640a5ea8318802ccea3940d4d0f8b5fe3
ucodecotz <ucode8888@gmail.com>
2020-12-07 01:10:46 +0300

    database and staticfiles

--- e_shell/settings.py
+++ e_shell/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'e_shell.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'e_shelldb',
+        'USER': 'totaltegeta',
+        'PASSWORD': 'Totaltegeta',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
